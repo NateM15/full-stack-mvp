458e7638e82c94047276568d3e4e41f695ac866a
NateM15 <natemorrison28@gmail.com>
2022-10-10 16:21:34 -0400

    Adds database, creates public folder

--- front-end/app.js
+++ front-end/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 3001
+const PORT = 3001;
+
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Online at ${PORT}`)
