1711f5ddda7ef5698b51dba409eeeb1696ce6503
NateM15 <natemorrison28@gmail.com>
2022-10-11 14:41:05 -0400

    Adds the create and delete ability

--- back-end/app.js
+++ back-end/app.js
@@ -15,7 +15,7 @@ pool.connect();
 
 app.use(cors());
 app.use(express.json());
-app.get('/guns/gun/:input', (req, res) => {
+app.get('/api/guns/gun/:input', (req, res) => {
     let input = req.params.input
     pool.query('SELECT * FROM guns RIGHT OUTER JOIN caliber ON caliber.caliber_id = guns.caliber_id WHERE caliber.caliber_id = guns.caliber_id AND name = $1',[`${input}`])
     .then(result => {
@@ -26,7 +26,7 @@ app.get('/guns/gun/:input', (req, res) => {
     });
 })
 
-app.get('/ammo/:input', (req, res) => {
+app.get('/api/ammo/:input', (req, res) => {
     let input = req.params.input
     pool.query('SELECT * FROM ammo RIGHT OUTER JOIN caliber ON caliber.caliber_id = ammo.caliber_id WHERE caliber.caliber_id = ammo.caliber_id AND name = $1',[`${input}`])
     .then(result => {
@@ -35,7 +35,34 @@ app.get('/ammo/:input', (req, res) => {
     .catch((error) => {
         console.error(error)
     });
+});
+//Shows info from favorites table
+app.get('/api/fav/page', (req, res) => {
+    pool.query('SELECT * FROM favorites')
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.error(error)
+    });
+});
+//Adds to favorites table
+app.post('/api/fav', (req, res) => {
+    let favInput = req.body;
+    pool.query('INSERT INTO favorites (gun_name, cal_name) VALUES ($1, $2)', [favInput.gun, favInput.caliber])
+    .then (result => {
+        res.status(201).send('Added Favorite')
+    });
+});
+//Deletes from favorites table
+app.delete('/api/delete/:delete', (req, res) => {
+    let deleted = req.params.delete
+    pool.query('DELETE FROM favorites WHERE gun_name = $1', [`${deleted}`])
+    .then(result => {
+        res.status(201).send('Deleted from Favorites')
+    });
 })
+
 
 app.listen(PORT, () => {
     console.log(`Liestening in on, ${PORT}`)
