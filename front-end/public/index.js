a8d1fc492775ca69a505be297c87ef2d5a3af156
NateM15 <natemorrison28@gmail.com>
2022-10-12 23:19:27 -0400

    Fixes name problem

--- front-end/public/index.js
+++ front-end/public/index.js
@@ -87,7 +87,7 @@ function addListener(button) {
             //Gets information from the div that the add to favorites button was pressed in
             selectedFav = e.target.parentElement;
             /**Note: Refactor front end and back end to support both guns and ammo favorites**/
-            sendData = {gun: `${selectedFav.children[0].textContent}`, caliber: `${selectedFav.children[1].textContent}`}
+            sendData = {gun: `${selectedFav.children[0].textContent[1]}`, caliber: `${selectedFav.children[1].textContent[4]}`}
             console.log(sendData)
             //Adds the information into the favorites table
             fetch(`${URL}fav`, {
@@ -128,7 +128,7 @@ function deleteButton(button){
     button.addEventListener('click', event => {
         content.innerHTML = "";
         let deleteBarValue = event.target.parentElement
-        console.log(deleteBarValue.children[0].textContent)
+        console.log(deleteBarValue.children[0].textContent[1])
         fetch(`${URL}delete/${deleteBarValue.children[0].textContent}`,
         {method: 'DELETE'})
         .then(alert('Delete Succesful'))
